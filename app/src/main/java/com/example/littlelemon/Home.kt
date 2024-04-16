175b2058c4e368892bbbbdf479fd4abf4995ee0c
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-04-16 20:51:18 +0530

    added navigation

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,10 +1,25 @@
 package com.example.littlelemon
 
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
 @Composable
 fun Home(navController: NavHostController) {
-    Text("Home")
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Home")
+        Button(onClick = { navController.navigate("profile")}) {
+            Text("Go to Profile")
+        }
+
+    }
 }
--- app/src/main/java/com/example/littlelemon/Profile.kt
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -1,10 +1,26 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
 @Composable
 fun Profile(navController: NavHostController) {
-    Text("Profile")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Profile")
+        Button(onClick = { navController.navigate("home")}) {
+            Text("Go to Home")
+        }
+
+    }
 }
