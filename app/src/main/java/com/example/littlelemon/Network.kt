d6c77fbe1ce339d8996c43d742493a42f41df46e
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-05-07 21:03:32 +0530

    Update Network.kt

--- app/src/main/java/com/example/littlelemon/Network.kt
+++ app/src/main/java/com/example/littlelemon/Network.kt
@@ -12,35 +12,12 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-//As soon as you get the JSON data from the server, you must convert it to a suitable Kotlin format. To do so, complete the following steps:
-//
-//Under the application package name, create a new file called Network.kt.
-//
-//In this file, create the MenuNetworkdata class and MenuItemNetwork data class with @Serializable and @SerialName annotations. These classes contain data classes that are used to decode the object received from the server. In the MainActivityclass, create the instance of the Ktor httpClient and install ContentNegotiation with JSON.
-//
-//Use the httpClient instance to make a network call and decode the MenuNetwork instance representing menu items from the server.
-//
-//Tip: The instance of the MenuNetwork class represents the entire menu. To retrieve individual menu items use the menu property of the MenuNetwork. Each item has id, title description, price and image attributes.
-
-//@Serializable
-//data class MenuNetworkData(
-//    @SerialName("menu") val menu: List<MenuItemNetwork>
-//)
-//
-//@Serializable
-//data class MenuItemNetwork(
-//    @SerialName("id") val id: MenuNetwork,
-//    @SerialName("title") val title: String,
-//    @SerialName("description") val description: String,
-//    @SerialName("price") val price: Double,
-//    @SerialName("image") val image: String
-//)
-
 @Serializable
 class MenuNetworkdata(
     @SerialName("menu")
     val menu: List<MenuItemNetwork>
 ){
+
 }
 
 @Serializable
@@ -58,4 +35,20 @@ data class MenuItemNetwork(
     @SerialName("category")
     val category: String
 )
+
+suspend fun fetchMenuData() : List<MenuItemNetwork> {
+    return try {
+        val httpClient = HttpClient(Android) {
+            install(ContentNegotiation) {
+                json(contentType = ContentType("text", "plain"))
+            }
+        }
+        val menuData : MenuNetworkdata = httpClient.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json").body()
+        Log.d("zd", menuData.menu.toString())
+        menuData.menu
+    } catch (e: Exception) {
+        Log.e("ze", e.message.toString())
+        emptyList()
+    }
+}
 
