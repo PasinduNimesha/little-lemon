cfeb73d798754f61d9e9602812a3c669b71f0ba3
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-05-09 23:14:02 +0530

    Update Home.kt

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,28 +1,99 @@
 package com.example.littlelemon
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun Home(navController: NavHostController) {
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text("Home")
-        Button(onClick = { navController.navigate("profile")}) {
-            Text("Go to Profile")
+//    Column (
+//        modifier = Modifier.fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ){
+//
+//        Text("Home")
+//        Button(onClick = { navController.navigate("profile")}) {
+//            Text("Go to Profile")
+//        }
+//        Button(onClick = { /*TODO*/ }) {
+//            Text(text = "Fetch Data")
+//        }
+//
+//    }
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo",
+            modifier = Modifier.padding(50.dp)
+        )
+        Button(
+            modifier = Modifier
+                .padding(start = 60.dp, end = 60.dp, top = 16.dp, bottom = 8.dp)
+                .fillMaxWidth(),
+            onClick = {
+
+            }) {
+            Text("Tap to Order By Name")
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Fetch Data")
-        }
+
+        var searchPhrase by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = searchPhrase,
+            label = { Text("Search") },
+            onValueChange = {
+                searchPhrase = it
+            })
+
 
     }
 }
