55b3a8b7095f8192f7b89a7750530c35b9974ea1
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-05-09 23:41:12 +0530

    Update Database.kt

--- app/src/main/java/com/example/littlelemon/Database.kt
+++ app/src/main/java/com/example/littlelemon/Database.kt
@@ -25,7 +25,7 @@ interface MenuItemDao {
     fun getAll(): LiveData<List<MenuItemRoom>>
 
     @Insert
-    fun insertAll(vararg menuItems: MenuItemRoom)
+    fun insertAll(vararg menuItems: List<MenuItemRoom>)
 
     @Query("SELECT (SELECT COUNT(*) FROM MenuItemRoom) == 0")
     fun isEmpty(): Boolean
