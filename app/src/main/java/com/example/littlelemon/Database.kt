eae430859e818aeae5defe61e9ce6e92b5c6ab56
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-05-10 16:24:39 +0530

    Update Database.kt

--- app/src/main/java/com/example/littlelemon/Database.kt
+++ app/src/main/java/com/example/littlelemon/Database.kt
@@ -9,14 +9,15 @@ import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.RoomDatabase
 
+
 @Entity
 data class MenuItemRoom(
     @PrimaryKey val id: Int,
     val title: String,
     val price: Double,
     val description: String,
+    val category: String,
     val image: String,
-    val category: String
 )
 
 @Dao
@@ -25,7 +26,7 @@ interface MenuItemDao {
     fun getAll(): LiveData<List<MenuItemRoom>>
 
     @Insert
-    fun insertAll(vararg menuItems: List<MenuItemRoom>)
+    fun insertAll(vararg menuItems: MenuItemRoom)
 
     @Query("SELECT (SELECT COUNT(*) FROM MenuItemRoom) == 0")
     fun isEmpty(): Boolean
