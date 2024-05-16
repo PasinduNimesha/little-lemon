da53642fda809eeacb4dabffddc02d12389545fa
Pasindu Rathnayaka <r.m.p.n.rathnayake@gmail.com>
2024-05-16 17:44:52 +0530

    Update Profile.kt

--- app/src/main/java/com/example/littlelemon/Profile.kt
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -10,7 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,6 +22,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -36,10 +41,17 @@ fun Profile(navController: NavHostController) {
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(painter = painterResource(id = R.drawable.img), contentDescription = "logo")
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "logo",
+            modifier = Modifier.width(200.dp),
+            contentScale = ContentScale.FillWidth
+        )
 
         Column(
-            modifier = Modifier.fillMaxWidth().padding(20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Box(
@@ -60,18 +72,17 @@ fun Profile(navController: NavHostController) {
             InputField(label = "Email", value = email.value, onValueChange = { email.value = it })
         }
 
-        Button(onClick = {
-            user.firstName = firstName.value.text
-            user.lastName = lastName.value.text
-            user.email = email.value.text
-            updateUser(navController.context, user)
-            navController.navigate("home")
-        }) {
-            Text("Save")
-        }
-
-        Button(onClick = { logOut(navController)}) {
-            Text("Log Out")
+        Column {
+            LongButton(text = "Save") {
+                user.firstName = firstName.value.text
+                user.lastName = lastName.value.text
+                user.email = email.value.text
+                updateUser(navController.context, user)
+                navController.navigate("home")
+            }
+            LongButton(text = "Log Out") {
+                logOut(navController)
+            }
         }
 
     }
